10ba1efbc9c98b3cfd82ed17e0f35b6e4829d0a3
tuantq <tranquoctuan24596@gmail.com>
2023-02-16 23:55:46 +0700

    [HRM] - Config database for production env

--- src/db/config/database.ts
+++ src/db/config/database.ts
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
-const host = process.env.DB_HOST;
-const username = process.env.DB_USERAME;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB_NAME;
+const host = process.env.NODE_ENV === "production" ? process.env.PGHOST : process.env.DB_HOST;
+const username = process.env.NODE_ENV === "production" ? process.env.PGUSER : process.env.DB_USERAME;
+const password = process.env.NODE_ENV === "production" ? process.env.PGPASSWORD : process.env.DB_PASSWORD;
+const database = process.env.NODE_ENV === "production" ? process.env.PGDATABASE : process.env.DB_NAME;
 const dialect = process.env.DB_DIALECT;
 
 module.exports = {
